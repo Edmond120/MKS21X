5bf58608778e47895681f2e95ee9fba3fb839d07
Edmond120 <ewong8@stuy.edu>
2016-12-07 17:36:50 -0500

    bug fixes for barcode

--- classwork/2016-12-7/Barcode.java
+++ classwork/2016-12-7/Barcode.java
@@ -1,6 +1,8 @@
-class runtimeException extends Exceptions{
+class runtimeException extends Throwable{
+    public String message;
     public runtimeException(String x){
-	System.out.println(x);
+	super();
+	message = x;
     }
 }
 public class Barcode implements Comparable<Barcode>{
@@ -21,20 +23,41 @@ public class Barcode implements Comparable<Barcode>{
     private int checkSum(){
 	int result = 0;
 	for(int i = 0; i < _zip.length(); i++){
-	    result += Interger.parseInt(_zip.charAt(i));
+	    result += Integer.parseInt(_zip.charAt(i) +"");
 	}
+	return result;
+    }
     public Barcode(String zip){
 	_zip = zip;
-	if(!precondition()){
-	    throw new runtimeException("zip is not the correct length or zip contrains a non digit, but I won't tell you which");
+	try{
+	    if(!precondition()){
+		throw new runtimeException("zip is not the correct length or zip contrains a non digit, but I won't tell you which");
+	    }
 	}
-	_checkDigit = checkSum(_zip) % 10;
+	catch (runtimeException e){
+	    System.out.println(e.message);
+	}
+	_checkDigit = checkSum() % 10;
     }
     public String toString(){
-	String result = _zip + "\n|";
+	String result = _zip + "  |";
 	for(int i = 0; i < _zip.length();i++){
 	    result = result + key[_zip.charAt(i)];
 	}
 	result = result + key[_checkDigit] + "|";
 	return result;
     }
+    public int compareTo(Barcode other){
+	int a = Integer.parseInt(_zip + _checkDigit);
+	int b = Integer.parseInt(other._zip + other._checkDigit);
+	if(a == b){
+	    return 0;
+	}
+	else if (a > b){
+	    return 1;
+	}
+	else{
+	    return -1;
+	}
+    }
+}
