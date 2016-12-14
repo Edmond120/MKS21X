ff98e3ad7a34505c777faf8f16cd06e95b807080
Edmond120 <ewong8@stuy.edu>
2016-12-14 05:54:21 -0500

    insertion sort

--- 04Sorts/Sorts.java
+++ 04Sorts/Sorts.java
@@ -17,6 +17,22 @@ public class Sorts{
 	    data[minIndex] = temp;
 	}
     }
+    public static void insertionSort(int[]data){
+	int temp;
+	for(int i = 0; i < data.length - 1; i++){
+	    if(! data[i] <= data[i + 1]){
+		temp = data[i + 1];
+		for(int i2 = 0; i2 <= i; i2++){
+		    if(temp <= data[i2]){
+			for(int i3 = i; i3 > i2; i3 = i3 - 1){
+			    data[i3] = data[i3 - 1];
+			}
+		    }
+		}
+	    }
+	}
+    }
+		
     public static String toString(int[]x){
 	String result = "[";
 	for(int i = 0;i < x.length;i++){
@@ -24,4 +40,9 @@ public class Sorts{
 	}
 	return result.substring(0,result.length() - 2) + "]";
     }
+    //   public static int[] toArray(String x){
+    //	int length = 0;
+    //	int[] result;
+    //	for(int i = 1; i < x.length() - 1; i++){
+	    
 }
