c8ecdf550dcfa97df12332abf289bd1f6ed22e56
Edmond120 <ewong8@stuy.edu>
2016-12-20 05:23:38 -0500

    homework done

---
+++ tempConverstion.java
@@ -0,0 +1,8 @@
+public class tempConverstion{
+    public static double cToF(double x){
+	return x * 9/5 + 32;
+    }
+    public static double fToC(double x){
+	return (x - 32) * 5/9;
+    }
+}
---
+++ x.java
@@ -0,0 +1,6 @@
+public class x{
+    public static void main(String[]args){
+	System.out.println(tempConverstion.cToF(Integer.parseInt(args[0])));
+	System.out.println(tempConverstion.fToC(Integer.parseInt(args[0])));
+    }
+}
